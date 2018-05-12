ae086442bb9e6127507df985fe14b4fe890fc55d
cameron4k <cameron4k@gmail.com>
2018-05-11 19:09:53 -0700

    commit changes to pull

--- public/views/images/Express App_files/app.js
+++ public/views/images/Express App_files/app.js
@@ -1,13 +1,9 @@
 var $ = window.$
-let voteCount = 3
 
 $(document).ready(function () {
   updateList()
-  //initially display the vote count to the user
-  displayVoteCount()
 })
 
-//handles user submission of song name search and passes to routes.js
 $('#sbmt').on('click', () => {
   event.preventDefault()
   var songName = $('#songName').val()
@@ -15,6 +11,8 @@ $('#sbmt').on('click', () => {
     songName: songName
   }
   $('#songName').val('')
+
+  console.log(songName)
 
   $.post('/song/create', data).then(function (r) {
     console.log(r)
@@ -24,11 +22,6 @@ $('#sbmt').on('click', () => {
     getVideos(r)
   })
 })
-
-//prints out the number of votes left to the user
-function displayVoteCount() {
-  $('.voteCountDiv').html('Votes Left : '+ voteCount)
-}
 
 function getSongs () {
   $.get('/all/videos').then(function (r) {
@@ -62,7 +55,7 @@ function getTable (songs) {
     $('#songs').append(`
     <tr>
     <th scope="row" class="align-middle">` + counter + `</th>
-    <td><button  id="upvote" data-value="` + songs[i].id + `" class="upvoteBtn bothVotes"><i class="fas fa-arrow-up"></i></button><br> <span class="">` + songs[i].votes + `</span> <br><button id="downvote" data-value="` + songs[i].id + `" class="downvoteBtn bothVotes"><i class="fas fa-arrow-down"></i></button></td>
+    <td><button  id="upvote" data-value="` + songs[i].id + `" class="upvoteBtn"><i class="fas fa-arrow-up"></i></button><br> <span class="">` + songs[i].votes + `</span> <br><button id="downvote" data-value="` + songs[i].id + `" class="downvoteBtn"><i class="fas fa-arrow-down"></i></button></td>
     <td><img src="` + songs[i].thumbnail_url + `"></td>
     <td class="text-left">` + songs[i].song_name + `</td>
     </tr>`
@@ -70,10 +63,13 @@ function getTable (songs) {
   }
 }
 
-//handles user voting on each song in the tableList, the variable voteCount is declared globally in line 2
+let voteCount = 3
+
 $(document).on('click', '#upvote', function () {
+  console.log('ping')
   var voteID = $(this).attr('data-value')
-  if (voteCount === 0) { alert('You are out of Votes! Please wait until the song is finished.') } else {
+  console.log(voteID)
+  if (voteCount === 0) { alert('you only get 3 votes') } else {
     $.ajax({
       url: '/song/upvote/' + voteID,
       type: 'PUT',
@@ -84,7 +80,6 @@ $(document).on('click', '#upvote', function () {
       }
     })
     voteCount--
-    displayVoteCount()
   }
 })
 
@@ -102,7 +97,6 @@ $(document).on('click', '#downvote', function () {
       }
     })
     voteCount--
-    displayVoteCount()
   }
 })
 
@@ -121,13 +115,6 @@ function onYouTubeIframeAPIReady () {
     height: '390',
     width: '640',
     videoId: 'VYOjWnS4cMY',
-    // playerVars: {
-    //   'controls': 0,
-    //   'disablekb': 1,
-    //   'iv_load_policy': 3,
-    //   'modestbranding': 1,
-    //   'rel': 0
-    // },
     events: {
       'onReady': onPlayerReady,
       'onStateChange': onPlayerStateChange
@@ -142,6 +129,7 @@ var index = -1
 
 function onPlayerReady (event) {
   event.target.playVideo()
+
 }
 
 // 5. The API calls this function when the player's state changes.
@@ -158,9 +146,9 @@ function onPlayerStateChange (event) {
         console.log(err)
       })
     voteCount = 3
-    displayVoteCount()
-    playNewVideo() 
-    nowPlaying() //updates to the user to display the song name and details of the song currently playing
+    playNewVideo()
+    nowPlaying()
+    
   }
 }
 
@@ -189,39 +177,23 @@ function updateList () {
 }
 
 // next video has the most votes
-function playNewVideo () {
-  $.get('/next/videos').then(function (songs) {
+function playNewVideo (id) {
+  $.get('/next/videos').then(function (r) {
+    var songs = r
     console.log(songs)
-    console.log(songs[0].video_id)
-    player.loadVideoById(songs[0].video_id)
+    console.log(songs[index].video_id)
+    player.loadVideoById(songs[index].video_id)
     event.target.playVideo()
     playNewVideo(nextID)
   })
 }
 
-// removes the song with the most votes that is called to play next
-function deletePlayingSong (id) {
-  $.ajax({
-    url: '/song/' + id,
-    type: 'DELETE',
-    success: function (result) {
-      // Do something with the result
-      console.log('This should be removed from the database' + result)
-      voteCount = 3
-      updateList()
-    }
-  })
-}
-
-// function to display the song name and details of the currently playing song in the youtube player iframe 
 function nowPlaying () {
   $.get('/next/videos').then(function (r) {
     var songs = r
-    console.log(r)
-    deletePlayingSong(r[0].id)
     $('#nowPlayingTable').html(`
-    <td><img src="` + songs[0].large_thumbnail_url + `"></td>
-    <td>` + songs[0].song_name + `</td>`)
+    <td><img src="` + songs[index].large_thumbnail_url + `"></td>
+    <td>` + songs[index].song_name + `</td>`)
   })
 }
 
@@ -229,29 +201,8 @@ function stopVideo () {
   player.stopVideo()
 }
 
-//admin login
-$('#admin').on('click', () => {
-  event.preventDefault()
-  var nameFront = $("#name").val()
-  var pwFront = $("#pw").val()
-  console.log(name)
-  console.log(pw)
-  $.get('/admin/creds').then(function(r){
-    var keys = r
-    console.log(keys[0].name)
-    var nameBack = keys[0].name
-    console.log(keys[0].password)
-    var pwBack = keys[0].password
-    if(nameFront === nameBack && pwFront === pwBack){
-      $("#admin").append("<h1>Hoorah</h1>")
-    }
-
-  })
-})
-
 // Working on a scroll to top button
 // $("#scrollUp").on('click', (function() {
 //   $("html, body").animate({ scrollTop: 0 }, "slow");
 //   return false;
 // }))
-
